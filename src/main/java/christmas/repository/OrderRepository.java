898a55bee79a8eed4a88b70cf3b90b7f8d1acf1a
sunJ0120 <sspure1214@gmail.com>
2026-01-04 09:54:27 +0900

    feat: 주문 관리를 위한 repository 구성 완료

--- src/main/java/christmas/repository/OrderRepository.java
+++ src/main/java/christmas/repository/OrderRepository.java
@@ -1,4 +1,22 @@
 package christmas.repository;
 
+import christmas.domain.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderRepository {
+    private final List<Order> orders;
+
+    public OrderRepository() {
+        this.orders = new ArrayList<>();
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void add(Order order) {
+        orders.add(order);
+    }
 }
