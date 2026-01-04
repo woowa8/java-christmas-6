48eb705ceb7e9e11f4140c5d2465796e28dc2840
sunJ0120 <sspure1214@gmail.com>
2026-01-04 09:52:59 +0900

    feat: parser 설정 완료

--- src/main/java/christmas/util/InputParser.java
+++ src/main/java/christmas/util/InputParser.java
@@ -1,4 +1,40 @@
 package christmas.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class InputParser {
+    public List<List<String>> parse(String input) {
+        List<List<String>> mainList = new ArrayList<>();
+
+        String[] lists = input.split(",");
+
+        for(String list : lists){
+            validateInput(list);
+            String[] parse = input.split("-");
+            validateNumber(Integer.parseInt(parse[1]));
+
+            List<String> subList = new ArrayList<>();
+            subList.add(parse[0]);
+            subList.add(parse[1]);
+
+            mainList.add(subList);
+        }
+        return mainList;
+    }
+
+    // 1. 해산물파스타-2 정규 표현식인지 검증
+    private void validateInput(String list) {
+        if(!list.matches("[가-힣]+-[0-9]+")){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateNumber(int number) {
+        if(number <= 0){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
 }
