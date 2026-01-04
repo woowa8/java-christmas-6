6800000807a8442900e61f278076c0e91f8503e7
sunJ0120 <sspure1214@gmail.com>
2026-01-04 09:54:06 +0900

    feat: 할인 관리를 위한 repository 구성 완료

--- src/main/java/christmas/repository/SaleRepository.java
+++ src/main/java/christmas/repository/SaleRepository.java
@@ -1,4 +1,32 @@
 package christmas.repository;
 
+import christmas.domain.Order;
+import christmas.domain.Sale;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SaleRepository {
+    private final List<Sale> totalSales;
+    private final Map<Sale, Integer> totalSaleCnt;
+
+    public SaleRepository() {
+        this.totalSales = new ArrayList<>();
+        this.totalSaleCnt = new HashMap<>();
+    }
+
+    public void addTotalSale(Sale sale) {
+        totalSales.add(sale);
+        totalSaleCnt.put(sale, totalSaleCnt.getOrDefault(sale, 0) + 1);
+    }
+
+    public List<Sale> findAllSales() {    // 전체 할인 금액을 계산하기 위해 필요
+        return totalSales;
+    }
+
+    public Map<Sale, Integer> findTotalSaleCnt() {
+        return totalSaleCnt;
+    }
 }
