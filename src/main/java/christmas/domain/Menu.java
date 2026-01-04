524a04d389a52f38909dd44b227a059e3ee44ca6
sunJ0120 <sspure1214@gmail.com>
2026-01-04 16:41:21 +0900

    feat: 기본 메뉴 틀 구성

--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -1,4 +1,25 @@
 package christmas.domain;
 
 public class Menu {
+    private final String name;
+    private Category category;
+    private int price;
+
+    public Menu(String name, Category category, int price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
