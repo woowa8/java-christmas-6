119c10d83e6a142ffb13cbd6343fdcbcc468febc
sunJ0120 <sspure1214@gmail.com>
2026-01-04 16:44:44 +0900

    feat: 실행을 위한 애플리케이션

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.controller.OrderController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OrderController controller = new OrderController();
+        controller.run();
     }
 }
