27f662d3cbde65aee5c72d1f208a8972cddfb949
sunJ0120 <sspure1214@gmail.com>
2026-01-04 09:52:42 +0900

    feat: view 설정 완료

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,48 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.util.InputParser;
+
+import java.util.List;
+import java.util.function.Supplier;
+
 public class InputView {
+    private final InputParser parser;
+
+    public InputView(InputParser parser) {
+        this.parser = parser;
+    }
+
+    // 식당 방문 날짜 받기
+    public int inputDay() {
+        return retryOnError(() -> {
+            System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            int input = Integer.parseInt(Console.readLine());
+
+            if (1 > input || input > 31) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+
+            return input;
+        });
+    }
+
+    // 주문 메뉴 받기
+    public List<List<String>> inputOrder() {
+        return retryOnError(() -> {
+            System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+            return parser.parse(Console.readLine());
+        });
+    }
+
+    private <T> T retryOnError(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,37 @@
 package christmas.view;
 
+import christmas.domain.Order;
+
+import java.util.List;
+
 public class OutputView {
+    // 총 주문 갯수 프린트
+    public void printOrders(List<Order> orders) {
+        System.out.println("<주문 메뉴>");
+        for (Order order : orders) {
+            System.out.println(order.getCount() + " " + order.getCount() + "개");
+        }
+        System.out.println();
+    }
+
+    // 할인 전 총주문 금액
+    public void printBeforeAmount(int amount) {
+        System.out.println("<할인 전 총주문 금액>");
+        String money = String.format("%,d", amount);
+        System.out.println(money + "원");
+    }
+
+    // 증정 메뉴
+    public void printPresent(String present) {
+        System.out.println("<증정 메뉴>");
+        System.out.println(present);
+    }
+
+    // 혜택 내역
+
+    // 총혜택 금액
+
+    // 할인 후 예상 결제 금액
+
+    // 12월 이벤트 배지
 }
