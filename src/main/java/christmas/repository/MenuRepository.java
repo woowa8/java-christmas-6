a4894c63f4f3ff5a2bb7bcff0b5a61b9317b6f11
sunJ0120 <sspure1214@gmail.com>
2026-01-04 09:55:13 +0900

    feat: 메뉴 관리를 위한 repository 생성 완료

--- src/main/java/christmas/repository/MenuRepository.java
+++ src/main/java/christmas/repository/MenuRepository.java
@@ -1,4 +1,28 @@
 package christmas.repository;
 
+import christmas.domain.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuRepository {
+    private List<Menu> menuList;
+
+    public MenuRepository() {
+        this.menuList = new ArrayList<>(menuList);
+    }
+
+    public void add(Menu menu) {
+        menuList.add(menu);
+    }
+
+    public List<Menu> getMenuList() {
+        return menuList;
+    }
+
+    public Menu findMenuByName(String menuName) {
+        return menuList.stream()
+                .filter(menu -> menu.getName().equals(menuName))
+                .findFirst().get();
+    }
 }
