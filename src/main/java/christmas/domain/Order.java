9075a00b6e637b396ebc90645360a7a529fbc1f6
sunJ0120 <sspure1214@gmail.com>
2026-01-04 16:41:38 +0900

    feat: 기본 주문 틀 구성

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,4 +1,19 @@
 package christmas.domain;
 
 public class Order {
+    private final Menu menu;
+    private int amount;
+
+    public Order(Menu menu, int amount) {
+        this.menu = menu;
+        this.amount = amount;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
